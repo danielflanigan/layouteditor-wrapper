42f908a150b8439d272d613acc5eeb7d814544a1
Daniel Flanigan <daniel.isaiah.flanigan@gmail.com>
2020-10-16 12:17:47 +0200

    Try to get conf.py to work on readthedocs 4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@
 import os
 import sys
 #sys.path.append(os.path.abspath('./source'))
-sys.path.append(os.path.abspath('../../source'))
+sys.path.insert(0, os.path.abspath('../../source'))
 print(sys.path)
 
 # -- Project information -----------------------------------------------------
@@ -79,14 +79,14 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'  #'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 html_theme_options = {
-    'fixed_sidebar': True,
+#    'fixed_sidebar': True,
 #    'page_width': None,
 }
 
