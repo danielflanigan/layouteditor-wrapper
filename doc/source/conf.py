b34aa327c05554b70707810aecb968bc84b3927d
Daniel Flanigan <daniel.isaiah.flanigan@gmail.com>
2020-10-16 08:35:29 +0200

    Try to get conf.py to work on readthedocs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -12,9 +12,8 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
 import sys
-sys.path.append(os.path.abspath('../../source'))
+sys.path.append('./source')
 
 
 # -- Project information -----------------------------------------------------
