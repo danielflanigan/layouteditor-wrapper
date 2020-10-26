ae00e21eeee23c9eb786eeb3e43d3fd156ca99ad
Daniel Flanigan <daniel.isaiah.flanigan@gmail.com>
2020-10-26 17:34:56 +0100

    .readthedocs.yml: attempt to install package 5.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -11,12 +11,12 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#import os
+import os
 import pkg_resources
-#import sys
+import sys
 
 # For ReadTheDocs
-#sys.path.insert(0, os.path.abspath('../../source'))
+sys.path.insert(0, os.path.abspath('../../source'))
 
 # -- Project information -----------------------------------------------------
 
